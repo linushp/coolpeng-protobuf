0c2566a18c2af66c390fc466739f32c7ffa1a43f
luanhaipeng <910010653@qq.com>
2016-11-01 11:59:34 +0800

    aaa

--- cp-register-api/src/main/java/com/coolpeng/grpc/register/api/innerimpl/forclient/GrpcServiceChannelUtils.java
+++ cp-register-api/src/main/java/com/coolpeng/grpc/register/api/innerimpl/forclient/GrpcServiceChannelUtils.java
@@ -2,7 +2,10 @@ package com.coolpeng.grpc.register.api.innerimpl.forclient;
 
 import com.coolpeng.grpc.register.api.model.GrpcServiceChannel;
 import com.coolpeng.grpc.register.api.model.GrpcServiceModel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -26,25 +29,43 @@ public class GrpcServiceChannelUtils {
         }
 
         //处理删除的
-        for(GrpcServiceChannel channel:managedChannelList){
-            if (!hasGrpcServiceModel(serviceModelList, channel.getGrpcServiceModel())){
-                managedChannelList.remove(channel);
+        Iterator<GrpcServiceChannel> it = managedChannelList.iterator();
+        while(it.hasNext()){
+            GrpcServiceChannel tempobj = it.next();
+            if(!hasGrpcServiceModel(serviceModelList, tempobj.getGrpcServiceModel())){
+                //移除当前的对象
+                it.remove();
             }
         }
-
     }
+
 
     private static boolean hasGrpcServiceModel(List<GrpcServiceModel> serviceModelList, GrpcServiceModel grpcServiceModel) {
 
-        //TODO
+        //处理新增的
+        for (GrpcServiceModel serviceModel : serviceModelList) {
+            if (grpcServiceModel.equals(serviceModel)){
+                return true;
+            }
+        }
+
         return false;
     }
 
 
     private static GrpcServiceChannel createGrpcServiceChannel(GrpcServiceModel serviceModel) {
 
-        //TODO
-        return null;
+        String host = serviceModel.getIp();
+        int port = serviceModel.getPort();
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext(true)
+                .build();
+
+        GrpcServiceChannel result = new GrpcServiceChannel();
+        result.setGrpcServiceModel(serviceModel);
+        result.setManagedChannel(channel);
+        return result;
     }
 
 
